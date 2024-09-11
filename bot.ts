10f80b8068e00f593d1047441294c745b4a55262
vetsinen <vetsinen@gmail.com>
2024-09-11 21:47:20 +0300

    init

---
+++ bot.ts
@@ -0,0 +1,10 @@
+import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+const env = await config();
+
+const bot = new Bot(env.TGTOKEN);// <-- put your bot token between the "" (https://t.me/BotFather)
+
+// Reply to any message with "Hi there!".
+bot.on("message", (ctx) => ctx.reply("start for deno tgbot!"));
+
+bot.start();
