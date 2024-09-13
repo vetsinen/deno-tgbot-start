b4b30d1d0cc2adfa63036c8a73049201eca95fee
vetsinen <vetsinen@gmail.com>
2024-09-13 12:52:27 +0300

    maybe takes from deno

--- bot.ts
+++ bot.ts
@@ -1,7 +1,15 @@
 import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-const env = await config();
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
-const bot = new Bot(env.TGTOKEN);// <-- put your bot token between the "" (https://t.me/BotFather)
+let botToken = Deno.env.get("TGTOKEN");
+if (!botToken) {
+     console.log("Deno Environment variable 'TGTOKEN' not found.");
+     const env = await config();
+     botToken = env.TGTOKEN
+    //Deno.exit()
+}
+console.log("Your bot token is:", botToken);
+
+const bot = new Bot(botToken);// <-- put your bot token between the "" (https://t.me/BotFather)
 bot.on("message", (ctx) => ctx.reply("start for deno tgbot!"));
 bot.start();
