3d244a474bbb6ea874f8359bab07d6ee0a81a0e0
vetsinen <vetsinen@gmail.com>
2024-09-13 12:30:24 +0300

    deleted .env

--- bot.ts
+++ bot.ts
@@ -2,10 +2,6 @@ import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const env = await config();
 
-console.log(env.TGTOKEN)
 const bot = new Bot(env.TGTOKEN);// <-- put your bot token between the "" (https://t.me/BotFather)
-
-// Reply to any message with "Hi there!".
 bot.on("message", (ctx) => ctx.reply("start for deno tgbot!"));
-
 bot.start();
